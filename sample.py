f1c15e4845e5e795e6cbad4e0208f7eb4e561d10
neelakshi atre <neelakshi.atre@enine.school>
2023-12-18 15:46:27 +0530

    committed

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
 def sums():
-     a = 4212
-     b = 18912512
-     total = sum(range(a,b))
-     print(total)
+    a = 4212
+    b = 18912512
+    total = sum(range(a, b))
+    print(total)
+
 
 sums()
